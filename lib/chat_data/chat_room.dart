741c83fe6601f0a7c710c4653ffe96b7f258db80
Usman <balawalsarao786@gmail.com>
2022-09-05 18:03:07 +0500

    code up to user message

--- lib/chat_data/chat_room.dart
+++ lib/chat_data/chat_room.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatRoomScreen extends StatefulWidget {
@@ -13,20 +14,55 @@ class ChatRoomScreen extends StatefulWidget {
 }
 
 class _ChatRoomScreenState extends State<ChatRoomScreen> {
-  TextEditingController sendController = TextEditingController();
 
+
+  TextEditingController sendController = TextEditingController();
   FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
 
   /// Send Message Function
-  void setMessageData(){
+  void setMessageData()async {
+    Map<String,dynamic> message={
+      "message" : sendController.text.trim(),
+      "sendby":FirebaseAuth.instance.currentUser!.email,
+      "time":FieldValue.serverTimestamp(),
+    };
+    await firebaseFirestore
+        .collection('UserChat')
+        .doc(widget.chatId)
+        .collection('chat')
+        .add(message);
 
+    sendController.clear();
   }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
           appBar: AppBar(
             title: Text(widget.userMap['name']),
+          ),
+          body:  StreamBuilder<QuerySnapshot>(
+            stream: firebaseFirestore
+                .collection('UserChat')
+                .doc(widget.chatId)
+                .collection('chat')
+                .orderBy('time',descending: false)
+                .snapshots(),
+            builder: (builder,snapShot){
+              if(!snapShot.hasData){
+                return Center(child: CircularProgressIndicator(),);
+              }else{
+                return Container(
+                  child: ListView.builder(
+                    itemCount: snapShot.data!.docs.length,
+                      itemBuilder: (context,index){
+                    return Text(snapShot.data!.docs[index]['message']);
+                  }
+                  ),
+                );
+              }
+            },
           ),
           bottomNavigationBar: Container(
             margin: EdgeInsets.all(10.0) ,
